cf28d4cfee6593e84191b05307eee1d58a124197
Steven He <hez2010@outlook.com>
2018-10-11 16:31:33 +0800

    upgrade npm packages

---
+++ hjudgeWeb/Controllers/GroupController.cs
@@ -0,0 +1,42 @@
+﻿using hjudgeWeb.Data;
+using hjudgeWeb.Data.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace hjudgeWeb.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly SignInManager<UserInfo> _signInManager;
+        private readonly UserManager<UserInfo> _userManager;
+        private readonly DbContextOptions<ApplicationDbContext> _dbContextOptions;
+
+        public GroupController(SignInManager<UserInfo> signInManager,
+            UserManager<UserInfo> userManager,
+            DbContextOptions<ApplicationDbContext> dbContextOptions)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _dbContextOptions = dbContextOptions;
+        }
+
+        private async Task<(UserInfo, int)> GetUserPrivilegeAsync()
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return (null, 0);
+            }
+            var user = await _userManager.GetUserAsync(User);
+            return (user, user?.Privilege ?? 0);
+        }
+
+        private bool HasAdminPrivilege(int privilege)
+        {
+            return privilege >= 1 && privilege <= 3;
+        }
+
+
+    }
+}
