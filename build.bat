f1ebd00b65a9adb35d426274ac3063f88cb2c121
Steven He <hez2010@outlook.com>
2019-02-05 22:48:56 +0800

    Add deploy script when possible

--- build.bat
+++ build.bat
@@ -24,3 +24,5 @@ set "INCLUDE=%cd%\lib\jsoncpp\include\json;!INCLUDE!"
 msbuild hjudgeExecWindows\hjudgeExecWindows.vcxproj /p:useEnv=true /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=!platformtoolversion!
 
 dotnet publish hjudgeWeb\hjudgeWeb.csproj -c Release
+
+if exists C:\hjudgeDeploy\deploy.bat C:\hjudgeDeploy\deploy.bat
