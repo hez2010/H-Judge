ab8edf51d846fccd1a523ce52cdd8bd7117d5db1
Steven He <hez2010@outlook.com>
2019-02-20 20:01:25 +0800

    update

--- hjudgeWebHost/ClientApp/src/App.tsx
+++ hjudgeWebHost/ClientApp/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Layout from './components/layout/layout'
+import Layout from './components/layout/layout';
+import 'semantic-ui-css/semantic.min.css';
 
 export default class App extends Component {
   render() {
@@ -8,7 +9,13 @@ export default class App extends Component {
       <div>
         <Layout>
           <Switch>
-            <Route exact path='/' render={() => { return <p>hhhh</p>; }}></Route>
+            <Route exact path='/' render={() => { return <p>home</p>; }}></Route>
+            <Route path='/problem' render={() => { return <p>problem</p>; }}></Route>
+            <Route path='/contest' render={() => { return <p>contest</p>; }}></Route>
+            <Route path='/group' render={() => { return <p>group</p>; }}></Route>
+            <Route path='/message' render={() => { return <p>message</p>; }}></Route>
+            <Route exact path='/about' render={() => { return <p>about</p>; }}></Route>
+            <Route render={() => {  return <p>404</p>; }}></Route>
           </Switch>
         </Layout>
       </div>
--- hjudgeWebHost/ClientApp/src/components/layout/layout.tsx
+++ hjudgeWebHost/ClientApp/src/components/layout/layout.tsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment
+} from 'semantic-ui-react';
+
 export default class Layout extends Component {
   render () {
     return (
       <div>
-        <h1>hello</h1>
-        <div>
+        <Menu fixed='top' inverted>
+          <Container>
+            <NavLink exact to='/' className='header item' isActive={() => false}>
+              <Image size='tiny' src='/assets/hjudge.png' style={{ marhinRight: '1.5em' }}></Image>
+            </NavLink>
+            <NavLink exact to='/' className='item'>主页</NavLink>
+            <NavLink to='/problem' className='item'>题库</NavLink>
+            <NavLink to='/contest' className='item'>比赛</NavLink>
+            <NavLink to='/group' className='item'>小组</NavLink>
+            <NavLink to='/message' className='item'>消息</NavLink>
+            <NavLink to='/about' className='item'>关于</NavLink>
+            <NavLink to='/login' className='right item'>登录</NavLink>
+          </Container>
+        </Menu>
+        <Container style={{ marginTop: '7em' }}>
             {this.props.children}
-        </div>
+        </Container>
       </div>
     );
   }
