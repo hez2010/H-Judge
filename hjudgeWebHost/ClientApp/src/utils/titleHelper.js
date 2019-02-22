5f1e118aeb02e3a220de8db7b522542e3afd5073
Steven He <hez2010@outlook.com>
2019-02-22 22:26:57 +0800

    remove unnecessary files

--- hjudgeWebHost/ClientApp/src/utils/titleHelper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function setTitle(title) {
-    document.title = title + ' - H::Judge';
-}
-exports.setTitle = setTitle;
-//# sourceMappingURL=titleHelper.js.map
