bc00ca46c83cb0ffef8dd114ef53359949c3b55b
Steven He <hez2010@outlook.com>
2019-03-05 13:56:23 +0800

    fix build script

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ set vsroot=%programfiles(x86)%\Microsoft Visual Studio
 for /f "tokens=*" %%a in ('dir /s /b /q "%vsroot%\VsDevCmd.bat"') do call "%%a" && goto :build
 
 :build
+dotnet restore
 msbuild hjudge.sln -m -verbosity:m /p:Platform=x64 /p:Configuration=Release
