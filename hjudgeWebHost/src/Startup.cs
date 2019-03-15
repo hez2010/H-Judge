ea5eb9d7cbd1be945387372189407b12e3ce3b93
Steven He <hez2010@outlook.com>
2019-03-15 14:20:55 +0800

    fixes

--- hjudgeWebHost/src/Startup.cs
+++ hjudgeWebHost/src/Startup.cs
@@ -91,7 +91,7 @@ namespace hjudgeWebHost
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (!env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
