f14d498a2ca17e93b4dda08979dc6dbcf2f56b99
Steven He <hez2010@outlook.com>
2019-04-10 10:22:44 +0800

    remove explict ctor

--- hjudgeWebHost/src/Configurations/ProblemConfig.cs
+++ hjudgeWebHost/src/Configurations/ProblemConfig.cs
@@ -6,26 +6,18 @@ namespace hjudgeWebHost.Configurations
 {
     public class ProblemConfig
     {
-        public ProblemConfig()
-        {
-            ExtraFiles = new List<string>();
-            Points = new List<DataPoint>();
-            ComparingOptions = new ComparingOption();
-            Answer = new AnswerPoint();
-            UseStdIO = true;
-        }
-
         public string SpecialJudge { get; set; } = string.Empty;
         public string InputFileName { get; set; } = string.Empty;
         public string OutputFileName { get; set; } = string.Empty;
         public string SubmitFileName { get; set; } = string.Empty;
-        public List<string> ExtraFiles { get; set; }
-        public List<DataPoint> Points { get; set; }
-        public AnswerPoint Answer { get; set; }
-        public ComparingOption ComparingOptions { get; set; }
-        public bool UseStdIO { get; set; }
+        public List<string> ExtraFiles { get; set; } = new List<string>();
+        public List<DataPoint> Points { get; set; } = new List<DataPoint>();
+        public AnswerPoint Answer { get; set; } = new AnswerPoint();
+        public ComparingOption ComparingOptions { get; set; } = new ComparingOption();
+        public bool UseStdIO { get; set; } = true;
         public string CompileArgs { get; set; } = string.Empty;
         public string Languages { get; set; } = string.Empty;
         public string ExtraFilesText => ExtraFiles.Aggregate(string.Empty, (accu, next) => accu + next + "\n");
+        public long CodeSizeLimit { get; set; }
     }
 }
