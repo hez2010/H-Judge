725fe76586e951a58857444f6522ec5fbc244ddc
Steven He <hez2010@outlook.com>
2019-09-09 14:05:04 +0800

    Upgrade fundamental of JudgeHost to use Worker

--- hjudge.FileHost/src/Program.cs
+++ hjudge.FileHost/src/Program.cs
@@ -1,30 +1,29 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace hjudge.FileHost
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureKestrel(kestrelOptions =>
-                    {
-                        // TODO: load port from config
-                        kestrelOptions.ListenLocalhost(61726,
-                            listenOptions =>
-                            {
-                                listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core
-                                    .HttpProtocols.Http2;
-                            });
-                    });
+                    webBuilder
+                        .ConfigureKestrel(kestrelOptions =>
+                        {
+                            // TODO: int.Parse(configuration["Host:Port"])
+                            kestrelOptions.ListenLocalhost(61726,
+                                listenOptions =>
+                                {
+                                    listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core
+                                        .HttpProtocols.Http2;
+                                });
+                        });
                 });
     }
 }
--- hjudge.WebHost/src/Program.cs
+++ hjudge.WebHost/src/Program.cs
@@ -7,7 +7,7 @@ namespace hjudge.WebHost
 {
     public class Program
     {
-        public static IServiceProvider? RootServiceProvider;
+        public static IServiceProvider RootServiceProvider;
         public static Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
