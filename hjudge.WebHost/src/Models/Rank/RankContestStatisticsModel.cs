6d8752675c0457f993ecbb8d1b9b5c1f4d6dac8d
Steven He <hez2010@outlook.com>
2020-02-23 17:17:29 +0800

    Fix docs for Rank

--- hjudge.WebHost/src/Models/Rank/RankContestStatisticsModel.cs
+++ hjudge.WebHost/src/Models/Rank/RankContestStatisticsModel.cs
@@ -7,16 +7,15 @@ namespace hjudge.WebHost.Models.Rank
         public int ContestId { get; set; }
         public int GroupId { get; set; }
         /// <summary>
-        /// 题目信息
+        /// 题目信息，Dictionary<int, RankProblemInfoModel>，key 为 ProblemId
         /// </summary>
-
         public Dictionary<int, RankProblemInfoModel> ProblemInfos = new Dictionary<int, RankProblemInfoModel>();
         /// <summary>
-        /// 用户信息，key 为 UserId
+        /// 用户信息，Dictionary<string, RankUserInfoModel>，key 为 UserId
         /// </summary>
         public Dictionary<string, RankUserInfoModel> UserInfos = new Dictionary<string, RankUserInfoModel>();
         /// <summary>
-        /// 排名信息，外部 key 为 UserId，内部 key 为题目 Id
+        /// 排名信息，Dictionary<string, Dictionary<int, RankContestItemModel>>，外部 key 为 UserId，内部 key 为 ProblemId
         /// </summary>
         public Dictionary<string, Dictionary<int, RankContestItemModel>> RankInfos { get; set; } = new Dictionary<string, Dictionary<int, RankContestItemModel>>();
     }
