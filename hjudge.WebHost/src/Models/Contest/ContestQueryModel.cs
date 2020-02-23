131227e0b5fb28770d52e143f2d8b607f442530e
Steven He <hez2010@outlook.com>
2020-02-23 18:08:26 +0800

    Fix routers for redoc and swagger

--- hjudge.WebHost/src/Models/Contest/ContestQueryModel.cs
+++ hjudge.WebHost/src/Models/Contest/ContestQueryModel.cs
@@ -3,6 +3,9 @@
     public class ContestQueryModel
     {
         public int ContestId { get; set; }
+        /// <summary>
+        /// 查询小组中的比赛，为 0 忽略
+        /// </summary>
         public int GroupId { get; set; }
     }
 }
