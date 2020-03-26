918134c8200915103fa4369096759b00c452a311
Steven He <hez2010@outlook.com>
2020-03-26 13:01:25 +0800

    Update rust interop interface

--- hjudge.Exec.Linux/src/lib.rs
+++ hjudge.Exec.Linux/src/lib.rs
@@ -1,10 +1,26 @@
 #![allow(non_snake_case)]
-#![no_mangle]
-use std::os::raw::c_char;
+use std::ffi::CStr;
 use std::ffi::CString;
-static mut STRING_POINTER: *mut c_char = 0 as *mut c_char;
+use std::os::raw::c_char;
+use std::slice;
 
-pub extern fn execute(param: &'static str, ret: &'static mut str) -> bool {
-    let pntr = CString::new(param).unwrap().into_raw();
-    return true;
+#[no_mangle]
+pub unsafe extern "C" fn execute(param: *const c_char, ret: *mut c_char) -> bool {
+    let input = CStr::from_ptr(param).to_string_lossy().into_owned();
+    let result = judge(&input);
+    let buffer = CString::new("test123456").unwrap().into_bytes_with_nul();
+    let slice = slice::from_raw_parts_mut(ret, result.0.len());
+    for index in 0..(result.0.len()) {
+        slice[index] = buffer[index] as i8;
+    }
+    return result.1;
+}
+
+fn judge(param: &str) -> (&str, bool) {
+    let jsonResult = json::parse(param);
+    if !jsonResult.is_ok() {
+        return ("Failed to parse JSON.", false);
+    }
+    let json = jsonResult.unwrap();
+    return ("test", true);
 }
